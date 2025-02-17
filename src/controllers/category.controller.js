656f30887f17f84f76968dbc6ef5819e3fcbe733
Kalana Didulanga Koralegedara <kalanadidulanga2005@gmail.com>
2025-02-17 15:31:06 +0530

    asdasd

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,27 +1,27 @@
-import { PrismaClient } from '@prisma/client';
-import { ApiResponse } from '../utils/apiResponse.js';
+import { PrismaClient } from "@prisma/client";
+import { ApiResponse } from "../utils/apiResponse.js";
 
 const prisma = new PrismaClient();
 
 export const getAllCategories = async (req, res) => {
-    try {
-        const categories = await prisma.category.findMany({
-            include: { 
-                packages: { include: { services: true } }
-             },
-        });
-        ApiResponse.success(res, categories);
-    } catch (error) {
-        ApiResponse.error(res, error.message, 500);
-    }
+  try {
+    const categories = await prisma.category.findMany({
+      include: {
+        packages: true,
+      },
+    });
+    ApiResponse.success(res, categories);
+  } catch (error) {
+    ApiResponse.error(res, error.message, 500);
+  }
 };
 
 export const createCategory = async (req, res) => {
-    try {
-        const { name } = req.body;
-        const category = await prisma.category.create({ data: { name } });
-        ApiResponse.success(res, category, 'Category created successfully', 201);
-    } catch (error) {
-        ApiResponse.error(res, error.message, 400);
-    }
+  try {
+    const { name } = req.body;
+    const category = await prisma.category.create({ data: { name } });
+    ApiResponse.success(res, category, "Category created successfully", 201);
+  } catch (error) {
+    ApiResponse.error(res, error.message, 400);
+  }
 };
